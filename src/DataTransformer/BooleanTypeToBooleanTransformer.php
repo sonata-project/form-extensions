51e084d5fa30f13b588cb2c293613c05f84081f7
Vincent Langlet <vincentlanglet@hotmail.fr>
2021-10-10 01:05:23 +0200

    Fix

--- src/DataTransformer/BooleanTypeToBooleanTransformer.php
+++ src/DataTransformer/BooleanTypeToBooleanTransformer.php
@@ -16,29 +16,32 @@ namespace Sonata\Form\DataTransformer;
 use Sonata\Form\Type\BooleanType;
 use Symfony\Component\Form\DataTransformerInterface;
 
-/**
- * @psalm-suppress InvalidReturnType, InvalidReturnStatement, FalsableReturnStatement
- *
- * @see https://github.com/psalm/psalm-plugin-symfony/issues/223
- */
 final class BooleanTypeToBooleanTransformer implements DataTransformerInterface
 {
+    /**
+     * @param mixed $value
+     */
     public function transform($value): ?int
     {
         if (true === $value || BooleanType::TYPE_YES === (int) $value) {
             return BooleanType::TYPE_YES;
-        } elseif (false === $value || BooleanType::TYPE_NO === (int) $value) {
+        }
+        if (false === $value || BooleanType::TYPE_NO === (int) $value) {
             return BooleanType::TYPE_NO;
         }
 
         return null;
     }
 
+    /**
+     * @param mixed $value
+     */
     public function reverseTransform($value): ?bool
     {
         if (BooleanType::TYPE_YES === $value) {
             return true;
-        } elseif (BooleanType::TYPE_NO === $value) {
+        }
+        if (BooleanType::TYPE_NO === $value) {
             return false;
         }
 
