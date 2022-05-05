03a44eb46cbb31fef404e98c98730cd9bb088984
Jordi Sala Morales <jordism91@gmail.com>
2022-05-05 08:44:40 +0200

    Drop Symfony 4 and Twig 2

--- src/Bridge/Symfony/Resources/config/validator.php
+++ src/Bridge/Symfony/Resources/config/validator.php
@@ -11,18 +11,17 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
 use Sonata\Form\Validator\InlineValidator;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->services()
-        // Use "service" function for creating references to services when dropping support for Symfony 4.4
         ->set('sonata.form.validator.inline', InlineValidator::class)
             ->tag('validator.constraint_validator', [
                 'alias' => 'sonata.form.validator.inline',
             ])
             ->args([
-                new ReferenceConfigurator('service_container'),
+                service('service_container'),
             ]);
 };
