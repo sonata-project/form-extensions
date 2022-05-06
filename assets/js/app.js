f2195d8ebbe701dbf23ca1a2193ee216de8694b0
Jordi Sala Morales <jordism91@gmail.com>
2022-05-06 16:25:35 +0200

    Add webpack and missing datepicker scripts

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+/*!
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// Any SCSS/CSS you require will output into a single css file (app.css in this case)
+import '../scss/app.scss';
+
+// Require jQuery normally
+import $ from 'jquery';
+
+import moment from 'moment';
+
+// Eonasdan Bootstrap DateTimePicker in its version 3 does not
+// provide the scss or plain css, it only provides the less version
+// of its source files, that's why it is not included it via npm.
+import '../vendor/bootstrap-datetimepicker.min';
+
+// Create global $ and jQuery variables to be used outside this script
+global.$ = $;
+global.jQuery = $;
+
+// Create global moment variable to be used by the locale script.
+// It expects moment to be available on the global scope
+// in order to define the requested locale translations
+global.moment = moment;
