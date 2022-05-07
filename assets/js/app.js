fd4fcb05291a0c348b3a94f309c45f20d1b0a893
Jordi Sala Morales <jordism91@gmail.com>
2022-05-07 11:09:21 +0200

    Try to not break jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,19 +10,12 @@
 // Any SCSS/CSS you require will output into a single css file (app.css in this case)
 import '../scss/app.scss';
 
-// Require jQuery normally
-import $ from 'jquery';
-
 import moment from 'moment';
 
 // Eonasdan Bootstrap DateTimePicker in its version 3 does not
 // provide the scss or plain css, it only provides the less version
 // of its source files, that's why it is not included it via npm.
 import '../vendor/bootstrap-datetimepicker.min';
-
-// Create global $ and jQuery variables to be used outside this script
-global.$ = $;
-global.jQuery = $;
 
 // Create global moment variable to be used by the locale script.
 // It expects moment to be available on the global scope
