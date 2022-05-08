46f5401c4724b2d0a81b01f5cb2746e81c6140ae
Jordi Sala Morales <jordism91@gmail.com>
2022-05-08 09:57:29 +0200

    Add webpack and missing datepicker scripts (#329)

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+/*!
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// Any SCSS/CSS you require will output into a single css file (app.css in this case)
+import '../scss/app.scss';
+
+import moment from 'moment';
+
+// Eonasdan Bootstrap DateTimePicker in its version 3 does not
+// provide the scss or plain css, it only provides the less version
+// of its source files, that's why it is not included it via npm.
+import '../vendor/bootstrap-datetimepicker.min';
+
+// Create global moment variable to be used by the locale script.
+// It expects moment to be available on the global scope
+// in order to define the requested locale translations
+global.moment = moment;
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+/*!
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const Encore = require('@symfony/webpack-encore');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+Encore.setOutputPath('./src/Bridge/Symfony/Resources/public')
+  .setPublicPath('.')
+  .setManifestKeyPrefix('bundles/sonataadmin')
+
+  .cleanupOutputBeforeBuild()
+  .enableSassLoader()
+  .enablePostCssLoader()
+  .enableVersioning(false)
+  .enableSourceMaps(false)
+  .enableEslintPlugin()
+  .autoProvidejQuery()
+  .disableSingleRuntimeChunk()
+
+  .addExternals({
+    jquery: 'jQuery',
+  })
+
+  .configureCssMinimizerPlugin((options) => {
+    options.minimizerOptions = {
+      preset: ['default', { discardComments: { removeAll: true } }],
+    };
+  })
+
+  .configureImageRule({
+    filename: 'images/[name][ext]',
+  })
+
+  .configureFontRule({
+    filename: 'fonts/[name][ext]',
+  })
+
+  .addPlugin(
+    new StyleLintPlugin({
+      context: 'assets/scss',
+      emitWarning: true,
+    })
+  )
+
+  .configureTerserPlugin((options) => {
+    options.terserOptions = {
+      output: { comments: false },
+    };
+    options.extractComments = false;
+  })
+
+  .copyFiles([{ from: './node_modules/moment/locale/', to: 'moment-locale/[name].[ext]' }])
+
+  .addEntry('app', './assets/js/app.js');
+
+module.exports = Encore.getWebpackConfig();
