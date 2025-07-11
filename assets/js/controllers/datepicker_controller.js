2d199d327d0c7e560bbf3b97c98b574ba7c41571
INTERNATIONALES WEB SERVICES <franckagbokoudjo301@gmail.com>
2025-07-11 23:09:11 +0100

    feat: update form fields for Bootstrap 5 (Twig + JS) and recompile public assets

--- assets/js/controllers/datepicker_controller.js
+++ assets/js/controllers/datepicker_controller.js
@@ -11,6 +11,20 @@ import { Controller } from '@hotwired/stimulus';
 import { TempusDominus, Namespace, loadLocale } from '@eonasdan/tempus-dominus';
 import { faFiveIcons } from '@eonasdan/tempus-dominus/dist/plugins/fa-five';
 
+/**
+ * @fileoverview
+ * Stimulus controller to manage the Tempus Dominus datetime picker widget,
+ * providing date/time selection with localization and configuration support.
+ *
+ * This controller supports dynamic locale loading, option processing,
+ * and synchronization between linked datepicker outlets.
+ *(c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ * @author Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ * @author AGBOKOUDJO Franck <internationaleswebservices@gmail.com>
+ * @license MIT
+ * @see https://github.com/Eonasdan/tempus-dominus
+ * @see https://stimulus.hotwired.dev/
+ */
 export default class extends Controller {
   static outlets = ['datepicker'];
 
@@ -36,6 +50,12 @@ export default class extends Controller {
 
   datePicker = null;
 
+  /**
+ * Called when the controller is connected to the DOM.
+ * Initializes the Tempus Dominus date/time picker with processed options and locale.
+ * Dispatches lifecycle events: 'pre-connect', 'post-connect-changed-locale', and 'connect'.
+ * @returns {void}
+ */
   connect() {
     const options = this.processOptions();
     const locale = this.processLocale(options);
@@ -58,6 +78,13 @@ export default class extends Controller {
     this.dispatchEvent('connect', { datePicker: this.datePicker });
   }
 
+  /**
+ * Called when a datepicker outlet is connected.
+ * Sets up event listeners to update minDate and maxDate restrictions between linked pickers.
+ * @param {HTMLElement} outlet - The outlet controller instance.
+ * @param {HTMLElement} element - The DOM element of the outlet.
+ * @returns {void}
+ */
   datepickerOutletConnected(outlet, element) {
     this.element.addEventListener(Namespace.events.change, (event) => {
       outlet.datePicker.updateOptions({
@@ -76,6 +103,11 @@ export default class extends Controller {
     });
   }
 
+  /**
+ * Processes the datetime picker options by converting string dates into Date objects,
+ * and sets default icons if not provided.
+ * @returns {Object} The processed options object ready to be passed to Tempus Dominus.
+ */
   processOptions() {
     const options = this.optionsValue;
     const { restrictions = {}, display = {} } = options;
@@ -111,6 +143,12 @@ export default class extends Controller {
     return options;
   }
 
+  /**
+ * Validates and extracts a supported locale from the given options.
+ * Returns a locale string if supported, or null if none is valid.
+ * @param {Object} options - Options that may contain localization settings.
+ * @returns {string|null} The locale string or null if unsupported.
+ */
   processLocale(options) {
     const { localization: { locale } = {} } = options;
 
@@ -135,6 +173,12 @@ export default class extends Controller {
     return null;
   }
 
+  /**
+ * Dispatches a custom event prefixed with 'datepicker' from this controller element.
+ * @param {string} name - The event name (without prefix).
+ * @param {Object} payload - The event detail payload.
+ * @returns {void}
+ */
   dispatchEvent(name, payload) {
     this.dispatch(name, { detail: payload, prefix: 'datepicker' });
   }
