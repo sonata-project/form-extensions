077e83afe4419ac74f10ff15ed51e703bd003e71
David Maicher <mail@dmaicher.de>
2025-10-30 20:42:51 +0100

    Fix Symfony 7.4 validator constraint deprecations (#560)

--- src/Validator/Constraints/InlineConstraint.php
+++ src/Validator/Constraints/InlineConstraint.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace Sonata\Form\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Exception\MissingOptionsException;
 
 /**
  * Constraint which allows inline-validation inside services.
@@ -26,15 +27,42 @@ use Symfony\Component\Validator\Constraint;
  */
 final class InlineConstraint extends Constraint
 {
-    protected mixed $service = null;
+    /**
+     * @param array<string, mixed>|null $options
+     */
+    public function __construct(
+        protected mixed $service = null, // NEXT_MAJOR: make private and non-nullable (and narrow the type?)
+        protected mixed $method = null, // NEXT_MAJOR: make private and non-nullable (and narrow the type?)
+        protected bool $serializingWarning = false, // NEXT_MAJOR: make private
+        ?array $groups = null,
+        ?array $options = null, // NEXT_MAJOR: remove
+    ) {
+        if (\is_array($service) || \is_array($options)) {
+            trigger_deprecation(
+                'sonata-project/form-extensions',
+                '2.6.0',
+                'Passing an array of options to configure the "%s" constraint is deprecated. Use named arguments instead.',
+                self::class,
+            );
 
-    protected mixed $method = null;
+            $options ??= [];
+            if (!\is_array($service)) {
+                $this->service = $service;
+            } else {
+                $options = array_merge($options, $service);
+            }
+            parent::__construct($options, groups: $groups);
+        } else {
+            $this->service = $service;
+            parent::__construct(groups: $groups);
+        }
 
-    protected bool $serializingWarning = false;
-
-    public function __construct(mixed $options = null)
-    {
-        parent::__construct($options);
+        if (null === $this->service || null === $this->method) {
+            throw new MissingOptionsException(
+                \sprintf('The required options/arguments "service" and "method" must be set for constraint "%s"', self::class),
+                ['service', 'method'],
+            );
+        }
 
         if ((!\is_string($this->service) || !\is_string($this->method)) && true !== $this->serializingWarning) {
             throw new \RuntimeException('You are using a closure with the `InlineConstraint`, this constraint'.
@@ -43,21 +71,31 @@ final class InlineConstraint extends Constraint
         }
     }
 
+    // TODO: remove when support for Symfony < 7.4 is dropped
     public function __sleep(): array
     {
         // @phpstan-ignore-next-line to initialize "groups" option if it is not set
         $this->groups;
 
-        if (!\is_string($this->service) || !\is_string($this->method)) {
+        if (!\is_string($this->getService()) || !\is_string($this->getMethod())) {
             return [];
         }
 
         return array_keys(get_object_vars($this));
     }
 
+    public function __serialize(): array
+    {
+        if (!\is_string($this->getService()) || !\is_string($this->getMethod())) {
+            return [];
+        }
+
+        return get_object_vars($this);
+    }
+
     public function __wakeup(): void
     {
-        if (\is_string($this->service) && \is_string($this->method)) {
+        if (\is_string($this->getService()) && \is_string($this->getMethod())) {
             return;
         }
 
@@ -74,12 +112,12 @@ final class InlineConstraint extends Constraint
 
     public function isClosure(): bool
     {
-        return $this->method instanceof \Closure;
+        return $this->getMethod() instanceof \Closure;
     }
 
     public function getClosure(): mixed
     {
-        return $this->method;
+        return $this->method ?? null;
     }
 
     public function getTargets(): string
@@ -87,22 +125,14 @@ final class InlineConstraint extends Constraint
         return self::CLASS_CONSTRAINT;
     }
 
-    public function getRequiredOptions(): array
-    {
-        return [
-            'service',
-            'method',
-        ];
-    }
-
     public function getMethod(): mixed
     {
-        return $this->method;
+        return $this->method ?? null;
     }
 
     public function getService(): mixed
     {
-        return $this->service;
+        return $this->service ?? null;
     }
 
     public function getSerializingWarning(): bool
