dcfad4061250b7804adfcc4ef494dbce215f8f63
Hans Mackowiak <h.mackowiak@b3-it.de>
2025-11-03 15:43:05 +0100

    Fix Webpack public path

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const Encore = require('@symfony/webpack-encore');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 Encore.setOutputPath('./src/Bridge/Symfony/Resources/public')
-  .setPublicPath('/bundles/sonataform')
+  .setPublicPath('.')
   .setManifestKeyPrefix('bundles/sonataform')
 
   .cleanupOutputBeforeBuild()
